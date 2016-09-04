29eea4265df914c47e01aa09733cbe0565d7ce36
X-Christy <1940124687@qq.com>
2016-09-05 07:59:32 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/test11/Main2Activity.java
@@ -0,0 +1,52 @@
+package cn.edu.bistu.cs.se.test11;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Intent intent=getIntent();
+        String name=intent.getStringExtra("name");
+        Integer age=intent.getIntExtra("age", 20);
+        Integer number=intent.getIntExtra("number", 2014011388);
+        Toast.makeText(this,name+"  "+age+"  "+number, Toast.LENGTH_LONG).show();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button button=(Button)findViewById(R.id.buttonReturn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=getIntent();
+                String name=intent.getStringExtra("name");
+                Integer age=intent.getIntExtra("age", 19);
+                Integer number=intent.getIntExtra("number", 2014011388);
+                intent.putExtra("result","姓名:"+name+"  年龄:"+age+"  学号:"+number);
+                setResult(0,intent);
+                finish();
+            }
+        });
+    }
+
+}
